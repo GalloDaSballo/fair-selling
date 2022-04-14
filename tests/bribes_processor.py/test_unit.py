6f6e5ab8f34344f6033bce0a97348d64e59f0dfa
Entreprenerd <valeranijr@gmail.com>
2022-04-14 23:58:38 +0200

    feat: progress in unit testing for bribes processing

--- tests/bribes_processor.py/test_unit.py
+++ tests/bribes_processor.py/test_unit.py
@@ -1,3 +1,7 @@
+import brownie
+from brownie import *
+from scripts.send_order import get_cowswap_order
+
 """
   Unit tests for all functions
   
@@ -26,3 +30,113 @@
     Works
     Emits event
 """
+
+
+### Ragequit
+
+def test_ragequit_permission(setup_processor, manager, usdc):
+  balance_before = usdc.balanceOf(setup_processor.BADGER_TREE())
+
+  ## The manager can RQ at any time
+  setup_processor.ragequit(usdc, False, {"from": manager})
+
+  assert usdc.balanceOf(setup_processor.BADGER_TREE()) > balance_before
+
+
+def test_ragequit_anyone_after_time(setup_processor, manager, usdc):
+  rando = accounts[2]
+
+  ## Reverts if rando calls immediately
+  with brownie.reverts():
+    setup_processor.ragequit(usdc, False, {"from": rando})
+
+  chain.sleep(setup_processor.MAX_MANAGER_IDLE_TIME() + 1)
+  chain.mine()
+
+  balance_before = usdc.balanceOf(setup_processor.BADGER_TREE())
+
+  ## Rando can call after time has passed
+  setup_processor.ragequit(usdc, False, {"from": rando})
+
+  assert usdc.balanceOf(setup_processor.BADGER_TREE()) > balance_before
+
+
+### Sell Bribes for Weth
+
+def test_sell_bribes_for_weth_cant_sell_badger(setup_processor, badger, weth, manager):
+  sell_amount = 10000000000000000000
+
+  order_details = get_cowswap_order(setup_processor, badger, weth, sell_amount)
+
+  data = order_details.order_data
+  uid = order_details.order_uid
+
+  with brownie.reverts():
+    setup_processor.sellBribeForWeth(data, uid, {"from": manager})
+
+def test_sell_bribes_for_weth_cant_sell_cvx(setup_processor, cvx, weth, manager):
+  sell_amount = 10000000000000000000
+
+  order_details = get_cowswap_order(setup_processor, cvx, weth, sell_amount)
+
+  data = order_details.order_data
+  uid = order_details.order_uid
+
+  with brownie.reverts():
+    setup_processor.sellBribeForWeth(data, uid, {"from": manager})
+
+
+def test_sell_bribes_for_weth_works_when_selling_usdc_for_weth(setup_processor, usdc, weth, manager, settlement):
+  sell_amount = 1000000000
+
+  order_details = get_cowswap_order(setup_processor, usdc, weth, sell_amount)
+
+  data = order_details.order_data
+  uid = order_details.order_uid
+
+  setup_processor.sellBribeForWeth(data, uid, {"from": manager})
+
+  assert settlement.preSignature(uid) > 0
+
+## TODO: must buy WETH
+
+## TODO: Can't sell WETH
+
+
+
+### Swap Weth for Badger
+
+def test_swap_weth_for_badger(setup_processor, weth, badger, manager, settlement):
+  sell_amount = 10000000000000000000
+
+  order_details = get_cowswap_order(setup_processor, weth, badger, sell_amount)
+
+  data = order_details.order_data
+  uid = order_details.order_uid
+
+  setup_processor.swapWethForBadger(data, uid, {"from": manager})
+
+  assert settlement.preSignature(uid) > 0
+
+
+## TODO: check that it must be WETH -> BADGER
+
+
+### Swap Weth for CVX
+
+def test_swap_weth_for_cvx(setup_processor, weth, cvx, manager, settlement):
+  sell_amount = 10000000000000000000
+
+  order_details = get_cowswap_order(setup_processor, weth, cvx, sell_amount)
+
+  data = order_details.order_data
+  uid = order_details.order_uid
+
+  setup_processor.swapWethForCVX(data, uid, {"from": manager})
+
+  assert settlement.preSignature(uid) > 0
+
+## TODO: check that it must be WETH -> CVX
+
+
+## swapCVXTobveCVXAndEmit
--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,8 @@ console = Console()
 DEV_MULTI = "0xB65cef03b9B89f99517643226d76e286ee999e77"
 WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
 USDC = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+USDC_HOLDER = "0x0a59649758aa4d66e25f08dd01271e891fe52199"
+
 
 ## Contracts ##
 @pytest.fixture
@@ -19,6 +21,11 @@ def seller(pricer):
   return CowSwapDemoSeller.deploy(pricer, {"from": a[0]})
 
 @pytest.fixture
+def processor(pricer):
+  return VotiumBribesProcessor.deploy(pricer, {"from": a[0]})
+
+
+@pytest.fixture
 def usdc():
   return interface.ERC20(USDC)
 
@@ -27,8 +34,43 @@ def weth():
   return interface.ERC20(WETH)
 
 @pytest.fixture
+def badger():
+  return interface.ERC20(WETH)
+
+@pytest.fixture
+def usdc_whale():
+  return accounts.at(USDC_HOLDER, force=True)
+
+@pytest.fixture
 def manager(seller):
   return accounts.at(seller.manager(), force=True)
+
+@pytest.fixture
+def strategy(processor):
+  return accounts.at(processor.STRATEGY(), force=True)
+
+@pytest.fixture
+def setup_processor(processor, strategy, usdc, usdc_whale):
+  ## Do the donation / Transfer Bribes
+  usdc.transfer(processor, 6e10, {"from": usdc_whale})
+
+  ## Notify new round, 28 days before anyone can unlock tokens
+  processor.notifyNewRound({"from": strategy})
+
+  return processor
+
+
+@pytest.fixture
+def badger(processor):
+  return interface.ERC20(processor.BADGER())
+
+@pytest.fixture
+def cvx(processor):
+  return interface.ERC20(processor.CVX())
+
+@pytest.fixture
+def settlement(processor):
+  return interface.ICowSettlement(processor.SETTLEMENT())
 
 ## Forces reset before each test
 @pytest.fixture(autouse=True)
