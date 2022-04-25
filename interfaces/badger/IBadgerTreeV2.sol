358b529f4c37327b92008e5f4dd43e27dfdbd67a
Entreprenerd <valeranijr@gmail.com>
2022-04-25 16:19:13 +0200

    feat: test with tree

--- interfaces/badger/IBadgerTreeV2.sol
+++ interfaces/badger/IBadgerTreeV2.sol
@@ -5,4 +5,21 @@ pragma experimental ABIEncoderV2;
 
 interface IBadgerTreeV2 {
     function lastPublishTimestamp() external view returns (uint256);
+
+    function proposeRoot(
+        bytes32 root,
+        bytes32 contentHash,
+        uint256 cycle,
+        uint256 startBlock,
+        uint256 endBlock
+    ) external; // onlyRootProposer
+
+    function approveRoot(
+        bytes32 root,
+        bytes32 contentHash,
+        uint256 cycle,
+        uint256 startBlock,
+        uint256 endBlock
+    ) external; // onlyRootValidator
+    
 }
--- tests/bribes_processor/test_emit.py
+++ tests/bribes_processor/test_emit.py
@@ -1,4 +1,5 @@
 import brownie
+from brownie import *
 import pytest
 
 
@@ -12,6 +13,18 @@ import pytest
     Sets new emissions schedule
     Emits event
 """
+
+@pytest.fixture
+def proposer():
+  return accounts.at("0x1a6d6d120a7e3f71b084b4023a518c72f1a93ee9", force=True)
+
+@pytest.fixture
+def approver():
+  return accounts.at("0x1318d5c0c24830d86cc27db13ced0ced31412438", force=True)
+
+@pytest.fixture
+def tree(setup_processor):
+  return BadgerTreeV2.at(setup_processor.BADGER_TREE())
 
 def test_swap_cvx_and_emit(rewards, setup_processor, manager, bve_cvx, cvx):
   bve_balance_before = bve_cvx.balanceOf(setup_processor.BADGER_TREE())
@@ -54,8 +67,7 @@ def test_emit_badger(rewards, setup_processor, manager, badger, bve_cvx):
 
 
 # note: currently does not work; needs a way to fake badger tree memory slot
-@pytest.mark.xfail
-def test_emit_badger_after_schedule_deadline(rewards, manager, bve_cvx, badger, badger_whale, setup_processor, bool_true):
+def test_emit_badger_after_schedule_deadline(tree, proposer, approver, rewards, manager, bve_cvx, badger, badger_whale, setup_processor):
   last_ending_time = rewards.getUnlockSchedulesFor(bve_cvx, badger)[-1][-2]
 
   # fast fwd chain to 1 day after last ending time
@@ -66,11 +78,35 @@ def test_emit_badger_after_schedule_deadline(rewards, manager, bve_cvx, badger, 
 
   # fast fwd badger tree last published timestamp
   # TODO: somehow fake BadgerTree.lastPublishTimestamp() to delta + 60 * 60 * 24
+  root = tree.merkleRoot()
+  contentHash = tree.merkleContentHash()
+  cycle = tree.currentCycle() + 1
+  startBlock = tree.lastPublishStartBlock() + 1
+  endBlock = tree.lastPublishEndBlock() + 500 ## About 500 blocks
+
+  tree.proposeRoot(
+    root,
+    contentHash,
+    cycle,
+    startBlock,
+    endBlock,
+    {"from": proposer}
+  )
+
+  tree.approveRoot(
+    root,
+    contentHash,
+    cycle,
+    startBlock,
+    endBlock,
+    {"from": approver}
+  )
+  
 
   # do another badger emissions, but now after schedule has ended already
   assert badger.balanceOf(setup_processor) > 0
   badger.transfer(setup_processor, 6e22, {"from": badger_whale})
-  test_emit_badger(rewards, setup_processor, manager, badger, bve_cvx, bool_true)
+  test_emit_badger(rewards, setup_processor, manager, badger, bve_cvx)
 
   # assert schedule set is indeed shorter than 14 days
   assert rewards.getUnlockSchedulesFor(bve_cvx, badger)[-1][-1] < 60 * 60 * 24 * 14
