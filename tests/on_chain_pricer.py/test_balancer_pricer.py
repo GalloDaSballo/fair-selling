74f214fdc649f3dcd9c959de96bc6a363ed62ab7
rayeaster <rayeaster@hotmail.com>
2022-06-13 18:50:37 +0800

    add sanity check for on-chain pricer using coingecko/coinmarketcap

--- tests/on_chain_pricer.py/test_balancer_pricer.py
+++ tests/on_chain_pricer.py/test_balancer_pricer.py
@@ -1,5 +1,10 @@
 import brownie
 from brownie import *
+
+import sys
+sys.path.append("C:\\ray\\code\\badgerdao-fair-selling\\scripts")
+from get_price import get_coingecko_price, get_coinmarketcap_price, get_coinmarketcap_metadata
+
 import pytest
 
 """
@@ -13,7 +18,12 @@ def test_get_balancer_price(oneE18, weth, usdc, pricer):
   ## minimum quote for ETH in USDC(1e6)
   p = 1 * 500 * 1000000  
   quote = pricer.getBalancerPrice.call(weth.address, sell_amount, usdc.address) 
-  assert quote >= p   
+  assert quote >= p 
+  
+  ## price sanity check with fine liquidity
+  p1 = get_coingecko_price('ethereum')
+  p2 = get_coingecko_price('usd-coin')
+  assert (quote / 1000000) >= (p1 / p2) * 0.98
   
 """
     getBalancerPriceWithConnector quote for token A swapped to token B with connector token C: A -> C -> B
@@ -21,12 +31,19 @@ def test_get_balancer_price(oneE18, weth, usdc, pricer):
 ## @pytest.mark.skip(reason="WBTC2USDC")
 def test_get_balancer_price_with_connector(oneE18, wbtc, usdc, weth, pricer):  
   ## 1e8
-  sell_amount = 10 * 100000000
+  sell_count = 10
+  sell_amount = sell_count * 100000000
     
   ## minimum quote for WBTC in USDC(1e6)
-  p = 10 * 10000 * 1000000  
+  p = sell_count * 10000 * 1000000  
   quote = pricer.getBalancerPriceWithConnector.call(wbtc.address, sell_amount, usdc.address, weth.address) 
-  assert quote >= p   
+  assert quote >= p    
+  
+  ## price sanity check with dime liquidity
+  yourCMCKey = 'b527d143-8597-474e-b9b2-5c28c1321c37'
+  p1 = get_coinmarketcap_price('3717', yourCMCKey) ## wbtc
+  p2 = get_coinmarketcap_price('3408', yourCMCKey) ## usdc
+  assert (quote / 1000000 / sell_count) >= (p1 / p2) * 0.75
   
 """
     getBalancerPrice quote for token A swapped to token B directly using given balancer pool: A - > B
