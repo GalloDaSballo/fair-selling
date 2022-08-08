f1b900641545e56bcfd78689930082f07ae22756
sajanrajdev <sajanrajdev10@gmail.com>
2022-08-08 15:41:52 -0400

    test: improved emit badger test

--- tests/aura_processor/test_emit_aura.py
+++ tests/aura_processor/test_emit_aura.py
@@ -68,14 +68,47 @@ def test_swap_aura_and_emit(setup_aura_processor, manager, aura, bve_aura):
     setup_aura_processor.swapAURATobveAURAAndEmit({"from": manager})
 
 
-def test_emit_badger(setup_aura_processor, manager, badger):
-  badger_balance_before = badger.balanceOf(setup_aura_processor.BADGER_TREE())
-  assert badger.balanceOf(setup_aura_processor) > 0
+def test_emit_badger(setup_aura_processor, manager, badger, bve_aura):
+  badger_tree_balance_before = badger.balanceOf(setup_aura_processor.BADGER_TREE())
+  badger_processor_balance_before = badger.balanceOf(setup_aura_processor)
+  badger_treasury_balance_before = badger.balanceOf(setup_aura_processor.TREASURY())
+  assert badger_processor_balance_before > 0
 
-  setup_aura_processor.emitBadger({"from": manager})
+  # Only manager can call
+  with brownie.reverts():
+    setup_aura_processor.emitBadger({"from": a[9]})
 
-  assert badger.balanceOf(setup_aura_processor.BADGER_TREE()) > badger_balance_before
+  tx = setup_aura_processor.emitBadger({"from": manager})
+
+  assert badger.balanceOf(setup_aura_processor.BADGER_TREE()) > badger_tree_balance_before
   assert badger.balanceOf(setup_aura_processor) == 0 ## All badger emitted
+
+  # Confirm math
+  ops_fee = int(badger_processor_balance_before) * OPS_FEE
+  # 1% approximation due to Brownie rounding
+  assert approx(ops_fee, badger.balanceOf(setup_aura_processor.TREASURY()) - badger_treasury_balance_before, 1)
+
+  to_emit = badger_processor_balance_before - ops_fee
+
+  # Confirm events
+  # Tree Distribution
+  assert len(tx.events["TreeDistribution"]) == 1
+  event = tx.events["TreeDistribution"][0]
+  assert event["token"] == badger.address
+  assert approx(event["amount"], to_emit, 1)
+  assert event["beneficiary"] == bve_aura.address
+
+  # Performance Fee Governance
+  assert len(tx.events["PerformanceFeeGovernance"]) == 1
+  event = tx.events["PerformanceFeeGovernance"][0]
+  assert event["token"] == badger.address
+  assert approx(event["amount"], ops_fee, 1)
+
+  # BribesEmission
+  assert len(tx.events["BribeEmission"]) == 1
+  event = tx.events["BribeEmission"][0]
+  assert event["token"] == badger.address
+  assert approx(event["amount"], to_emit, 1)
 
   ## Reverts if called a second time
   with brownie.reverts():
