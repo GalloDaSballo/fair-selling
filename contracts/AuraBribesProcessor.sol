0b143125abc23f7a8576c9a6ee24aea8be86ed6e
Entreprenerd <alex@entreprenerd.xyz>
2022-08-08 18:03:19 +0200

    feat: 5% fee everywhere + cleanup

--- contracts/AuraBribesProcessor.sol
+++ contracts/AuraBribesProcessor.sol
@@ -47,11 +47,8 @@ contract AuraBribesProcessor is CowSwapSeller {
 
     // Source: https://badger.com/graviaura
     uint256 public constant MAX_BPS = 10_000;
-    uint256 public constant BADGER_SHARE = 2500; //25.00%
-
     // A 5% fee will be charged on all bribes processed.
     uint256 public constant OPS_FEE = 500; // 5%
-    // uint256 public constant LP_FEE = 0; // Not used
 
     /// `treasury_voter_multisig`
     /// https://github.com/Badger-Finance/badger-multisig/blob/6cd8f42ae0313d0da33a208d452370343e7599ba/helpers/addresses.py#L52
@@ -191,7 +188,7 @@ contract AuraBribesProcessor is CowSwapSeller {
         uint256 totalBveAURA = BVE_AURA.balanceOf(address(this));
         require(totalBveAURA > 0);
 
-        uint256 ops_fee = totalBveAURA * OPS_FEE / (MAX_BPS - BADGER_SHARE);
+        uint256 ops_fee = totalBveAURA * OPS_FEE / MAX_BPS;
         IERC20(address(BVE_AURA)).safeTransfer(TREASURY, ops_fee);
 
         // Subtraction to avoid dust
@@ -213,10 +210,14 @@ contract AuraBribesProcessor is CowSwapSeller {
 
         // Sends Badger to the Tree
         // Emits custom event for it
-        uint256 toEmitTotal = BADGER.balanceOf(address(this));
-        require(toEmitTotal > 0);
+        uint256 totalBadger = BADGER.balanceOf(address(this));
+        require(totalBadger > 0);
 
-        BADGER.safeApprove(address(HARVEST_FORWARDER), toEmitTotal);
+        uint256 ops_fee = totalBadger * OPS_FEE / MAX_BPS;
+        IERC20(address(BADGER)).safeTransfer(TREASURY, ops_fee);
+
+        uint256 toEmitTotal = totalBadger - ops_fee;
+        BADGER.safeIncreaseAllowance(address(HARVEST_FORWARDER), toEmitTotal);
         HARVEST_FORWARDER.distribute(address(BADGER), toEmitTotal, address(BVE_AURA));
 
         emit BribeEmission(address(BADGER), address(BVE_AURA), toEmitTotal);
